204449d8dfd91e3c24d22add050e078df58da17e
BugHunter-X <askzenai@gmail.com>
2025-02-03 05:48:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a paragraph of text.</p>
+    </div>
+  );
+}
+```
+This simple Next.js app might unexpectedly throw an error if the `pages` directory isn't structured correctly or if there's a problem with the JSX syntax.  It's an uncommon error because it's so basic, but it highlights the importance of a clean project structure and accurate JSX.
